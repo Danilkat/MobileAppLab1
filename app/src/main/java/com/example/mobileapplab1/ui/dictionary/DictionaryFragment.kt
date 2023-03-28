35a2b7601de771523144c7a58b9c21006382f844
Danil Katashev <katashnya@gmail.com>
2023-03-28 23:51:08 +0700

    dictionary api - meaning, name, ...

--- app/src/main/java/com/example/mobileapplab1/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/example/mobileapplab1/ui/dictionary/DictionaryFragment.kt
@@ -1,15 +1,21 @@
 package com.example.mobileapplab1.ui.dictionary
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mobileapplab1.*
 import com.example.mobileapplab1.databinding.FragmentDictionaryBinding
+import com.squareup.moshi.Moshi
+import retrofit2.converter.moshi.MoshiConverterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
 
 class DictionaryFragment : Fragment() {
 
@@ -19,32 +25,46 @@ class DictionaryFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dictionaryViewModel =
-            ViewModelProvider(this).get(DictionaryViewModel::class.java)
 
         _binding = FragmentDictionaryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val dictionaryMeaningItemsAdapter = DictionaryMeaningItemsAdapter(
-            listOf(
-                DictionaryMeaningItem(
-                    meaning = "The practice or skill of preparing food by combining, mixing, and heating ingredients. \n" +
-                            "Example: he developed an interest in cooking.",
-                    meaningExample = ""
-                ),
-                DictionaryMeaningItem(
-                    meaning = "The practice or skill of preparing food by combining, mixing, and heating ingredients. \n" +
-                            "Example: he developed an interest in cooking.",
-                    meaningExample = ""
-                )
-            )
+        val moshi = Moshi.Builder()
+            .add(com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory())
+            .build()
 
-        )
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.dictionaryapi.dev/api/v2/entries/en/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        val service = retrofit.create(DictionaryApi::class.java)
+        val meaningItems: MutableList<DictionaryMeaningItem> = mutableListOf()
+        val dictionaryMeaningItemsAdapter = DictionaryMeaningItemsAdapter(meaningItems)
+
+
+        binding.textInputLayoutCookingSearch.setEndIconOnClickListener {
+            val word: String = binding.dictionaryInputCooking.text.toString()
+            lifecycleScope.launch(Dispatchers.IO) {
+                val result = service.getWordMeaning(word)[0]
+                withContext(Dispatchers.Main) {
+                    binding.textWord.text = result.word
+                    binding.textPhonetic.text = result.phonetics[0].transcription
+                    binding.textPartOfSpeech.text = result.meanings[0].partOfSpeech
+                    meaningItems.clear()
+                    for (definition in result.meanings[0].definitions) {
+                        meaningItems += DictionaryMeaningItem(definition.definition, definition.example)
+                    }
+                    dictionaryMeaningItemsAdapter.notifyDataSetChanged()
+                }
+            }
+        }
 
         binding.recyclerViewDictionary.layoutManager = LinearLayoutManager(root.context)
         binding.recyclerViewDictionary.adapter = dictionaryMeaningItemsAdapter
