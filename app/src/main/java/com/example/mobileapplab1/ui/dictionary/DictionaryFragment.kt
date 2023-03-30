f8f4eb6df2986c60187bc496689f2e6236cf0496
Danil Katashev <katashnya@gmail.com>
2023-03-30 18:23:38 +0700

    database code

--- app/src/main/java/com/example/mobileapplab1/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/example/mobileapplab1/ui/dictionary/DictionaryFragment.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.mobileapplab1.*
 import com.example.mobileapplab1.databinding.FragmentDictionaryBinding
 import com.squareup.moshi.Moshi
@@ -54,41 +55,57 @@ class DictionaryFragment : Fragment() {
 
         mediaPlayer = MediaPlayer()
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-        var playerAvailable = false;
+        var playerAvailable = false
+
+        val db = Room.databaseBuilder(
+            binding.root.context,
+            DictionaryDatabase::class.java, "dictionary-db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+        var result: WordInformation? = null
 
         binding.textInputLayoutCookingSearch.setEndIconOnClickListener {
             val word: String = binding.dictionaryInputCooking.text.toString()
-            playerAvailable = false;
             lifecycleScope.launch(Dispatchers.IO) {
-                val result: WordInformation;
                 try {
                     result = service.getWordMeaning(word)[0]
-                } catch (e: retrofit2.HttpException) {
-                    e.printStackTrace()
-                    withContext(Dispatchers.Main) {
-                        AlertDialog.Builder(binding.root.context)
-                            .setTitle(getString(R.string.dictionary_not_found_title))
-                            .setMessage(R.string.dictionary_not_found_desc)
-                            .show()
+                } catch (e: Exception) {
+                    val wordandMeanings = db.dictionaryDao().findByWord(word)
+                    if (wordandMeanings == null) {
+                        withContext(Dispatchers.Main) {
+                            AlertDialog.Builder(binding.root.context)
+                                .setTitle(getString(R.string.dictionary_not_found_title))
+                                .setMessage(R.string.dictionary_not_found_desc)
+                                .show()
+                        }
+                        return@launch
                     }
-                    return@launch;
+                    val definitionORMItems = mutableListOf<Definition>()
+                    for (meaning in wordandMeanings.meanings) {
+                        definitionORMItems += Definition(meaning.meaning, meaning.example)
+                    }
+                    result = WordInformation(
+                        wordandMeanings.word.name,
+                        listOf(Phonetics(wordandMeanings.word.transcription,"")),
+                        listOf(Meaning(wordandMeanings.word.partOfSpeech!!, definitionORMItems)))
                 }
                 withContext(Dispatchers.Main) {
-
-                    if (result.phonetics[0].audio != "") {
-                        mediaPlayer.setDataSource(result.phonetics[0].audio)
+                    playerAvailable = false
+                    if (result!!.phonetics[0].audio != "") {
+                        mediaPlayer.setDataSource(result!!.phonetics[0].audio)
                         mediaPlayer.prepareAsync()
-                        playerAvailable = true;
+                        playerAvailable = true
                     }
-                    binding.textWord.text = result.word
-                    binding.textPhonetic.text = result.phonetics[0].transcription
-                    binding.textPartOfSpeech.text = result.meanings[0].partOfSpeech
+                    binding.textWord.text = result!!.word
+                    binding.textPhonetic.text = result!!.phonetics[0].transcription
+                    binding.textPartOfSpeech.text = result!!.meanings[0].partOfSpeech
                     meaningItems.clear()
-                    for (definition in result.meanings[0].definitions) {
+                    for (definition in result!!.meanings[0].definitions) {
                         meaningItems += DictionaryMeaningItem(definition.definition, definition.example)
                     }
                     dictionaryMeaningItemsAdapter.notifyDataSetChanged()
-
 
                 }
             }
@@ -104,6 +121,30 @@ class DictionaryFragment : Fragment() {
         binding.recyclerViewDictionary.adapter = dictionaryMeaningItemsAdapter
 
 
+
+        binding.button.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                if (result != null) {
+                    val nnResult = result!!
+                    val word = WordEntity(
+                        nnResult.word,
+                        nnResult.phonetics[0].transcription,
+                        nnResult.meanings[0].partOfSpeech,
+                        nnResult.phonetics[0].audio
+                    )
+                    db.dictionaryDao().insertWord(word)
+                    for (definition in result!!.meanings[0].definitions) {
+                        db.dictionaryDao().insertMeaning(
+                            MeaningEntity(
+                                nnResult.word,
+                                definition.definition,
+                                definition.example
+                            )
+                        )
+                    }
+                }
+            }
+        }
 
         return root
     }
